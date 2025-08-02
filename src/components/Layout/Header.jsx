1e729f9bab0836a33b72bd2af4f83d2bf0f6503d
alhamrimohamed <alhamrimohammed4@gmail.com>
2025-08-02 12:49:52 +0300

    creat Header and Layout components

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import { logout } from "../../services/authAPI";
+
+const Header = () => {
+    const token = localStorage.getItem("access_token");
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    const handleLogout = async () => {
+        await logout();
+    }
+
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header-content">
+                    <Link to="/" className="logo">
+                        Event Planner
+                    </Link>
+
+                    {token && (
+                        <nav className="nav-links">
+                            <li><Link to="/dashboard">Dashboard</Link></li>
+                            <li><Link to="/events">All Events</Link></li>
+                            <li><Link to="/my-events">My Events</Link></li>
+                            <li><Link to="attending">Attending</Link></li>
+                            <li><Link to="/create-event">Create Event</Link></li>
+                        </nav>
+                    )}
+                    <div className="auth-buttons">
+                        {token ? (
+                            <>
+                                <Link to="/profile" className="btn btn-secondary">
+                                    {user ? user.username : "Profile"}
+                                </Link>
+                                <button className="btn btn-primary" onClick={handleLogout}>
+                                    Logout
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/login" className="btn btn-secondary">
+                                    Login
+                                </Link>
+                                <Link to="/signup" className="btn btn-primary">
+                                    Signup
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Header />
+            <main className="main-content">
+                <div className="container">
+                    {children}
+                </div>
+            </main>
+        </div>
+    );
+}
