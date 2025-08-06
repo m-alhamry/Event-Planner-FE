0c97770ea7b82f9a93893a678120c086e5d266c4
alhamrimohamed <alhamrimohammed4@gmail.com>
2025-08-06 15:45:00 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,      // Ensure this matches your expected port
+  },
+  // Other config options
+});
